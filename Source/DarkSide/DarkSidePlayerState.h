823dd837c79bcbbb52cad0eb86e560e19e0fd76e
garodon21aque <64726247+garodon21aque@users.noreply.github.com>
2023-03-03 22:49:44 +0000

    First Update
    Reorganised everything for preproduction
    
    -Folders
    -Base Components for programming

---
+++ Source/DarkSide/DarkSidePlayerState.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "DarkSidePlayerState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DARKSIDE_API ADarkSidePlayerState : public APlayerState
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/DarkSide/FrameWork/DarkSideGameInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "DarkSideGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DARKSIDE_API UDarkSideGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
